cbf48c8e7d275ce30cbb4b5bcac7d7e832a94e89
SmansabosCurhatinAja <javrayhanalt2006@gmail.com>
2024-01-19 16:49:37 +0700

    SmansabosCurhatinAja

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.menu_button').click(function(){
+        $('.menu').toggleClass("slide-menu-tampil");
+    });
+});
+
+$(document).ready(function(){
+    $('.menu_button2').click(function(){
+        $('.menu').toggleClass("slide-menu-tampil");
+    });
+});
